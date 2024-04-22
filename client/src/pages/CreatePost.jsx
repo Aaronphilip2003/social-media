41ad7de377e7b17c43848e71402b674b4cc653f9
Aaronphilip2003 <aaronphilip2003@gmail.com>
2024-04-22 15:14:08 +0530

    Corrected File image upload section

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -8,8 +8,8 @@ export default function CreatePost() {
     <div className='p-3 max-w-3xl mx-auto min-h-screen'>
         <h1 className='text-center text-3xl my-7 font-semibold'>Create Post</h1>
         <form className='flex flex-col gap-4'>
-            <div className='flex flex-col gap-4 sm:flex-row justify-between '>
-                <TextInput type="text" placeholder='Title' required id="title" className='lg'/>
+            <div className='flex gap-4'>
+                <TextInput type="text" placeholder='Title' required id="title" className='flex-1'/>
                 <Select>
                     <option value="uncategorized">Select a Category</option>
                     <option value="personaldevelopment">Personal Development</option>
@@ -18,10 +18,10 @@ export default function CreatePost() {
                     <option value="confidencebuilding">Confidence Building</option>
                     <option value="motivationandinspiration">Motivation and Inspiration</option>
                 </Select>
-                <div className='flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3'>
-                    <FileInput type='file' accept='image/*'/>
-                    <Button type='button' gradientDuoTone='purpleToBlue' size='sm' outline>Upload Image</Button>
-                </div>
+            </div>
+            <div className='flex gap-4 items-center border-4 border-teal-500 border-dotted p-3'>
+                <FileInput type='file' accept='image/*'/>
+                <Button type='button' gradientDuoTone='purpleToBlue' size='sm' outline>Upload Image</Button>
             </div>
             <ReactQuill theme="snow" placeholder='Write your post here...' className='h-72 mb-12' required/>
             <Button type='submit' gradientDuoTone='purpleToPink'>Publish</Button>
