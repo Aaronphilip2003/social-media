355803a929f4edf72513c7664ec11ce37ea358af
Aaronphilip2003 <aaronphilip2003@gmail.com>
2024-05-02 11:01:21 +0530

    create get posts api route

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,9 +1,11 @@
 import { verifyToken } from "../utils/verifyUser.js";
 import express from "express"
 import { create } from "../controllers/post.controller.js";
+import { getposts } from "../controllers/post.controller.js";
 
 const router = express.Router();
 
 router.post('/create',verifyToken,create)
+router.get('/getposts',getposts)
 
 export default router;
