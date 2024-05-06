71e83103387a53178b2ee7eb037be578f4964759
Aaronphilip2003 <aaronphilip2003@gmail.com>
2024-05-07 00:20:50 +0530

    Completed About and Project pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
-
 export default function About() {
   return (
-    <div>
-      About
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='max-w-2xl mx-auto p-3 text-center'>
+        <div>
+          <h1 className='text-3xl font font-semibold text-center my-7'>
+            About ACTSPOT
+          </h1>
+          <div className='text-md text-gray-500 flex flex-col gap-6'>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat sint error deleniti cumque accusamus! Fuga, optio? Accusantium officia iure quos consequatur fuga, voluptatibus illo eveniet quo amet sapiente maiores facere culpa sed eius in, eum neque tempora odit. Voluptates quidem rerum optio labore maiores hic quasi eius vel eligendi fugit?
+            </p>
+
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi optio id, voluptas, dolorum odio impedit recusandae neque sequi molestias, ducimus unde alias ipsam accusantium deserunt. Facere provident, quos magni exercitationem minus consequatur quas? Obcaecati sint dolor quaerat laborum velit perferendis deserunt doloribus nobis animi. Similique impedit ipsam explicabo quam magnam.
+            </p>
+
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum excepturi, magni assumenda sunt totam, accusamus tenetur voluptates impedit neque cupiditate autem facere! Incidunt deserunt voluptas tempora dolores doloremque voluptates, laudantium fuga quis rem, ipsam eius necessitatibus totam nisi consequuntur doloribus vitae corrupti nesciunt molestias voluptate quia. Optio deserunt modi voluptas?
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
